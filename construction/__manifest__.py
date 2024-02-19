373591e96a1675b98a1c2da27aed0aa681f7b013
adch-odoo <adch@odoo.com>
2024-02-19 16:29:24 +0530

    [ADD] construction : first commit

---
+++ construction/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Construction Management",
+    "version": "0.1",
+    "depends": ["base"],
+    "author": "adch@odoo.com",
+    "category": "Localization",
+    "description": """
+    The One and Only Best Real Estate Availble on Odoo!
+    """,
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "license": "LGPL-3",
+}
