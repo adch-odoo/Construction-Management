4f966aa37b0030898efc7e9d92bc7cc9cfb5dea2
adch-odoo <adch@odoo.com>
2024-02-21 18:45:27 +0530

    [ADD] construction: added models menus and views
    Implemented two models: construction_management_tags and
    construction_management_offer.
    - Created a many2many relational field in construction_management
    to relate with tags.
    - Established a one2many field to interact with offers.
    - Utilized a compute function to calculate start and end dates based on
    contractor's expected start date and tenure by client.
    - Implemented ordering functionalities.
    - Provided a search view for enhanced usability.

--- construction/__manifest__.py
+++ construction/__manifest__.py
@@ -5,13 +5,18 @@
     "author": "adch@odoo.com",
     "category": "Localization",
     "description": """
-    The One and Only Best Real Estate Availble on Odoo!
+    The One and Only Best Construction Management Availble on Odoo!
     """,
     "installable": True,
     "application": True,
     "auto_install": False,
     "data": [
         "security/ir.model.access.csv",
+        "data/construction_management_sequence.xml",
+        "views/construction_management_tag_views.xml",
+        "views/construction_management_offer_views.xml",
+        "views/construction_management_views.xml",
+        "views/construction_menus.xml"
     ],
     "license": "LGPL-3",
 }
---
+++ construction/models/__init__.py
@@ -0,0 +1,3 @@
+from . import construction_management
+from . import construction_management_offer
+from . import construction_management_tag
